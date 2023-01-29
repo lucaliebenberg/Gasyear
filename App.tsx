bbd5c32f3867281e19ffd713f3479298eacdb240
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-01-29 16:03:07 +0200

    onboarding add

--- App.tsx
+++ App.tsx
@@ -2,13 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 // import Home from "./src/screens/Home";
-import Blog from "./src/screens/Blog";
+// import Blog from "./src/screens/Blog";
+import OnboardingCarousel from "./src/screens/OnboardingCarousel";
 
 export default function App() {
   return (
     <View>
       <SafeAreaView>
-        <Blog />
+        <OnboardingCarousel />
       </SafeAreaView>
     </View>
   );
---
+++ src/screens/OnboardingCarousel.tsx
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+import Onboarding from "react-native-onboarding-swiper";
+
+const OnboardingCarousel = () => (
+  <View style={styles.container}>
+    <Onboarding
+      pages={[
+        {
+          backgroundColor: "#fff",
+          image: <Image source={require("../../assets/onboard1.png")} />,
+          title: "Take a picture",
+          subtitle: "Done with React Native Onboarding Swiper",
+        },
+        {
+          backgroundColor: "#fff",
+          image: <Image source={require("../../assets/onboard2.png")} />,
+          title: "Get your history",
+          subtitle: "Done with React Native Onboarding Swiper",
+        },
+        {
+          backgroundColor: "#fff",
+          image: <Image source={require("../../assets/onboard3.png")} />,
+          title: "Purchase analysis",
+          subtitle: "Done with React Native Onboarding Swiper",
+        },
+      ]}
+    />
+  </View>
+);
+
+export default OnboardingCarousel;
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    marginTop: 680,
+    color: "black",
+  },
+});
