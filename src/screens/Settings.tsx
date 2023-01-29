15d754e4c927ce5dee472a1e904edaed56e513d1
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-01-29 20:30:55 +0200

    settings screen work on + settings profile nav links

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -1,14 +1,91 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+
+// import images
+import UserCar from "../../assets/settings-car-image.png";
+import SettingsNotificationIcon from "../../assets/notification-settings-icon.png";
+import LogOutIcon from "../../assets/log-out-icon.png";
+import EditProfileIcon from "../../assets/edit-icon.png";
 
 const Settings = () => {
   return (
-    <View>
-      <Text>Settings</Text>
+    <View style={styles.container}>
+      <View style={styles.userInfoContainer}>
+        <View style={styles.userInfo}>
+          <Image source={UserCar} style={styles.userInfoImage} />
+          <Text style={styles.userEmail}>thomasb@gmail.com</Text>
+          <Text style={styles.userLocation}>South Africa</Text>
+        </View>
+      </View>
+      <View style={styles.settingsInfoContainer}>
+        <View style={styles.settingsInfo}>
+          <Image
+            source={SettingsNotificationIcon}
+            style={styles.settingsInfoIcon}
+          />
+          <Text style={styles.settingsInfoText}>Notifications</Text>
+        </View>
+        <View style={styles.settingsInfo}>
+          <Image source={EditProfileIcon} style={styles.settingsInfoIcon} />
+          <Text style={styles.settingsInfoText}>Edit Profile</Text>
+        </View>
+        <View style={styles.settingsInfo}>
+          <Image source={LogOutIcon} style={styles.settingsInfoIcon} />
+          <Text style={styles.settingsInfoText}>Log Out</Text>
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Settings
+export default Settings;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  userInfoContainer: {
+    paddingTop: 40,
+  },
+  userInfo: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  userInfoImage: {
+    width: 250,
+    height: 170,
+  },
+  userEmail: {
+    fontSize: 22,
+    paddingTop: 18,
+  },
+  userLocation: {
+    fontSize: 14,
+    color: "grey",
+    paddingTop: 10,
+  },
+  settingsInfoContainer: {
+    display: "flex",
+    flexDirection: "column",
+    paddingTop: 120,
+    paddingRight: 40,
+  },
+  settingsInfo: {
+    display: "flex",
+    flexDirection: "row",
+    paddingBottom: 40,
+    // borderBottomWidth: 2,
+    // borderBottomColor: "grey",
+  },
+  settingsInfoIcon: {
+    width: 24,
+    height: 24,
+    marginRight: 20,
+  },
+  settingsInfoText: {
+    fontSize: 20,
+    marginLeft: 20,
+  },
+});
